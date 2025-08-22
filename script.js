686f1aa959a606601d3783666bf3d6e6a64bcc5c
Davy Nyakinyua <davynyakingua@gmail.com>
2025-08-22 12:41:23 +0300

    Add possibe moves a knight can make on the board

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('script.js');
+
+// function that shows the shortest path to a vertex
+function knightMoves (start, end){
+    // Define the possible moves a knight can make
+    const knightMoves = [
+        [2, 1], [2, -1], [-2, 1], [-2. -1],
+        [-1, 2], [1, 2], [-1, -2], [1, -2]
+    ];
+}
